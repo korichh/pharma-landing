485fd49e8279c7b27abe9c068fa7f371cb651dd5
korGetAv <korichhak@gmail.com>
2024-04-17 14:13:22 +0300

    zhdu pravki :)

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,15 +1,19 @@
 (function() {
+    const header = document.querySelector('.header')
     const scrollTop = document.querySelector('.scroll-top')
     const mobileMenu = document.querySelector('.mobile-menu')
+    const selectLang = document.querySelector('.select-lang')
 
-    if (scrollTop) {
+    if (header && scrollTop) {
         window.addEventListener('scroll', checkScroll)
         checkScroll()
 
         function checkScroll() {
             if (scrollY > 40) {
+                header.classList.add('_active')
                 scrollTop.classList.add('_active')
             } else {
+                header.classList.remove('_active')
                 scrollTop.classList.remove('_active')
             }
         }
@@ -24,9 +28,83 @@
         })
 
         mobileMenu.addEventListener('click', (e) => {
+            if (e.target.closest('.menu-item')) {
+                mobileMenu.classList.remove('_active')
+            }
             if (e.target.closest('.close') || !e.target.closest('.mobile-menu__inner')) {
                 e.preventDefault()
                 mobileMenu.classList.remove('_active')
+            }
+        })
+    }
+
+    if (selectLang) {
+        selectLang.addEventListener('click', (e) => {
+            if (e.target.closest('.select-lang__selected')) selectLang.classList.add('_active');
+            selectLang.addEventListener('mouseleave', () => {
+                selectLang.classList.remove('_active')
+            }, {
+                once: true
+            })
+        })
+    }
+
+    if ('select') {
+        document.addEventListener('click', (e) => {
+            if (e.target.closest('.select')) {
+                e.preventDefault()
+                if (e.pointerId < 0) return
+                const select = e.target.closest('.select')
+                const option = e.target.closest('.select__options > button')
+                if (option) {
+                    const selected = select.querySelector('.select__selected-value')
+                    const options = select.querySelectorAll('.select__options > button')
+                    selected.innerHTML = escapeHtml(option.innerHTML)
+                    options.forEach(el => el.classList.remove('_active'))
+                    option.classList.add('_active')
+                    select.classList.remove('_active')
+                } else {
+                    select.classList.add('_active')
+                    select.addEventListener('mouseleave', () => {
+                        select.classList.remove('_active')
+                    }, {
+                        once: true
+                    })
+                }
+            }
+        })
+    }
+
+    if ('count') {
+        document.addEventListener('click', (e) => {
+            if (e.target.closest('.count')) {
+                e.preventDefault()
+                const count = e.target.closest('.count')
+                const btn = e.target.closest('.count__btn')
+                const input = count.querySelector('.count__input')
+                const min = +input.getAttribute('min')
+                const max = +input.getAttribute('max')
+                const value = +input.value
+                if (!btn) return
+                if (btn.classList.contains('count__decr')) {
+                    if (value <= min) return input.value = min
+                    input.value = value - 1
+                } else {
+                    if (value >= max) return input.value = max
+                    input.value = value + 1
+                }
+            }
+        })
+        document.addEventListener('input', (e) => {
+            if (e.target.closest('.count')) {
+                e.preventDefault()
+                if (e.inputType === 'deleteContentBackward' || e.inputType === 'deleteContentForward') return
+                const input = e.target.closest('.count__input')
+                const min = +input.getAttribute('min')
+                const max = +input.getAttribute('max')
+                const value = +input.value
+                if (value > max) input.value = max
+                if (value < min) input.value = min
             }
         })
     }
@@ -45,6 +123,33 @@
                 }
             }
         })
+    }
+
+    if ('change price') {
+        document.addEventListener('click', (e) => {
+            if (e.target.closest('.select__options button') || e.target.closest('.count__btn')) updatePrice(e)
+        })
+        document.addEventListener('input', (e) => {
+            if (e.target.closest('.count__input')) updatePrice(e)
+        })
+
+        function updatePrice(e) {
+            const form = e.target.closest('.order-form')
+            const priceDisplay = form.querySelector('.order-form__price')
+            const currency = priceDisplay.getAttribute('data-currency')
+            const price = form.querySelector('.select__options button._active') ? form.querySelector('.select__options button._active').getAttribute('data-price') : priceDisplay.getAttribute('data-price')
+            const count = form.querySelector('.count__input').value || 1
+            priceDisplay.innerHTML = `${(price * count).toFixed(2)} ${currency}`
+        }
+    }
+
+    function escapeHtml(html) {
+        return html
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
     }
 })()
 
